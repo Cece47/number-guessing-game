73c46fedb4f6991783e4910cabc4f605514ae792
Colleen Carroll <leenmcarroll@gmail.com>
2015-05-13 16:26:13 -0400

    initial commit

---
+++ number-guessing.js
@@ -0,0 +1,19 @@
+var randomNumber = (Math.floor(Math.random()* 10) ) + 1;
+var txt = document.querySelector('.number');
+var btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+  var guess = parseInt(txt.value);
+  console.log (guess);
+  alert(compare(guess, randomNumber));
+});
+
+function compare (guess, randomNumber) {
+  if (guess > randomNumber) {
+    return 'You guessed too high. Try again.';
+  } else if (guess > randomNumber) {
+    return 'You guessed too low. Try again.';
+  } else if (guess == randomNumber) {
+    return 'You won!';
+  }
+}
